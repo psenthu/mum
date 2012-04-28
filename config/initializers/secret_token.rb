7727fa92b4054d41a399c7149f1e98fdc491a882
Senthuraan Ponnamapalam <psenthu@gmail.com>
2012-04-28 15:00:59 +0530

    Rails app with twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mum::Application.config.secret_token = '67d88e1f421123d2aea62f5a1ae9172029503f0790ab7d0b1d05479187794d0b52349f5fa6a66ec5cc79f501cf051eacabaa29468e442daf9cbcb24227eab0bd'
