3521752c76cac1805747376788e881d8d7fc9081
pragashonlink <pragashonlink@gmail.com>
2012-05-02 22:26:07 +0530

    module which handles all the money transactions

---
+++ lib/handle_transaction.rb
@@ -0,0 +1,80 @@
+module HandleTransaction
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
+
+  def fund_available?
+    fund_for_user > self.fund.abs
+  end
+
+  def fund_for_user
+    Transaction.where("user_id = ?", self.user_id).
+                sum("fund")
+  end
+
+  module ClassMethods
+    def process_transaction(params)
+      deduct = nil
+      add = nil
+
+      if params[:type] == "TF"
+        Transaction.transaction do
+          deduct = deduct_credit(params)
+          add = add_credit(params)
+        end
+      elsif params[:type] == "AC"
+        add = add_credit(params)
+      elsif params[:type] == "DC"
+        deduct = deduct_credit(params)
+      end
+
+      render_ws_response(deduct, add)
+    end
+
+    def add_credit(params)
+      receiver = Transaction.new() do |r|
+        r.user_id = params[:to_user_id] ||= params[:user_id]
+        r.account_id = ActsAsTenant.current_tenant.id
+        r.type = params[:type]
+        r.info = params[:info]
+        r.fund = params[:fund]
+        r.operation = "add"
+      end
+      receiver.save
+
+      receiver
+    end
+
+    def deduct_credit(params)
+      deducter = Transaction.new() do |r|
+        r.user_id = params[:user_id]
+        r.account_id = ActsAsTenant.current_tenant.id
+        r.type = params[:type]
+        r.info = params[:info]
+        r.fund = "-#{params[:fund]}".to_f
+        r.operation = "deduct"
+      end
+      deducter.save
+
+      deducter
+    end
+
+    private
+
+    def render_ws_response(*args)
+      responses = []
+
+      args.each do |o|
+        next if o.nil? || o.errors.nil?
+
+        o.errors.each do |k,v|
+          responses << v
+        end
+      end
+
+      responses
+    end
+  end
+
+end
