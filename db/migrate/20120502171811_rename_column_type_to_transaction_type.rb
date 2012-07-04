952ee6731dab48c9007f68b012d6eb978a083ecd
Senthuraan Ponnamapalam <psenthu@gmail.com>
2012-07-04 11:46:46 +0530

    migration file error fixed, still test needs to be modified to have it pass

--- db/migrate/20120502171811_rename_column_type_to_transaction_type.rb
+++ db/migrate/20120502171811_rename_column_type_to_transaction_type.rb
@@ -1,9 +1,9 @@
 class RenameColumnTypeToTransactionType < ActiveRecord::Migration
   def up
-    rename_column :type, :transaction_type
+    rename_column :transactions, :type, :transaction_type
   end
 
   def down
-    rename_column :transaction_type, :type
+    rename_column :transactions, :transaction_type, :type
   end
 end
